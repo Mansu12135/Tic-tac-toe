64019424a29ad2ea89e00a7e2381cbc47ddf57bd
Nikita <nik_1996@i.ua>
2017-06-07 21:21:16 +0300

    fix figure designe

--- Tic-tac-toe/Tic-tac-toe/PlayingField.cs
+++ Tic-tac-toe/Tic-tac-toe/PlayingField.cs
@@ -19,7 +19,7 @@ namespace Tic_tac_toe
         private MainPage Page;
         private double Width;
         private TicTacToeMatrix TicTac;
-        public PlayingField(MainPage page,int size, double width)
+        public PlayingField(MainPage page, int size, double width)
         {
             Page = page;
             Size = size;
@@ -33,14 +33,12 @@ namespace Tic_tac_toe
             var verticalPanel = new StackPanel();
             verticalPanel.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Stretch;
             verticalPanel.VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Stretch;
-
             for (int i = 0; i < Size; i++)
             {
                 var horizPanel = new StackPanel();
                 horizPanel.Orientation = Orientation.Horizontal;
                 horizPanel.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Stretch;
                 horizPanel.VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Stretch;
-                // horizPanel.PointerEntered += HorizPanel_PointerEntered;
                 for (int j = 0; j < Size; j++)
                 {
                     Border bordResource;
@@ -52,7 +50,7 @@ namespace Tic_tac_toe
                             offset = new Vector3(6, 6, 0);
                             bordResource = ((Border)Page.Resources["BorderTopLeft"]);
                         }
-                        else if(j == Size - 1)
+                        else if (j == Size - 1)
                         {
                             offset = new Vector3(-6, 6, 0);
                             bordResource = ((Border)Page.Resources["BorderTopRight"]);
@@ -64,7 +62,7 @@ namespace Tic_tac_toe
                             bordResource = ((Border)Page.Resources["BorderTop"]);
                         }
                     }
-                    else if (i== Size - 1)
+                    else if (i == Size - 1)
                     {
                         if (j == 0)
                         {
@@ -102,11 +100,6 @@ namespace Tic_tac_toe
                             bordResource = ((Border)Page.Resources["BorderCenter"]);
                         }
                     }
-
-                    //  bord.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Stretch;
-                    //   bord.VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Stretch;
-                    //   bord.BorderThickness = new Windows.UI.Xaml.Thickness(1);
-                    //  bord.BorderBrush = new SolidColorBrush(Colors.Aqua);
                     var bord = new Border() { CornerRadius = bordResource.CornerRadius, BorderBrush = bordResource.BorderBrush, BorderThickness = bordResource.BorderThickness };
                     bord.Height = sideSize;
                     bord.Width = sideSize;
@@ -122,7 +115,7 @@ namespace Tic_tac_toe
                     canv.Background = new SolidColorBrush(Colors.Transparent);
                     bord.Child = canv;
                     horizPanel.Children.Add(bord);
-                   // SetShadow(offset, canv, sideSize-12);
+                    // SetShadow(offset, canv, sideSize-12);
                 }
                 verticalPanel.Children.Add(horizPanel);
             }
@@ -131,7 +124,7 @@ namespace Tic_tac_toe
         private void SetShadow(Vector3 offset, Canvas c, double size)
         {
             var hostVisual = ElementCompositionPreview.GetElementVisual(c);
-           var  compositor = hostVisual.Compositor;
+            var compositor = hostVisual.Compositor;
 
             //// Create a drop shadow
             var dropShadow = compositor.CreateDropShadow();
@@ -150,54 +143,44 @@ namespace Tic_tac_toe
             //// Add the shadow as a child of the host in the visual tree
             ElementCompositionPreview.SetElementChildVisual(c, shadowVisual);
         }
-        public bool Tic = true;
+        public bool Tic = false;
+        string point;
         private void Canv_PointerPressed(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
         {
             var canv = sender as Canvas;
             if (canv == null) return;
             if (canv.Tag == null) return;
-            if (canv.Children.Count == 1) return;
+            if (canv.Children.Count == 1)
+            {
+                canv.Children.Clear();
+            }
+            var pp = e.GetCurrentPoint(canv);
+            point += "'" + pp.Position.X + "-" + pp.Position.Y + "' ";
             var split = canv.Tag.ToString().Split('-');
             int i = Convert.ToInt32(split[0]);
             int j = Convert.ToInt32(split[1]);
-            //var bord = new Border();
-            //bord.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Center;
-            //bord.VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Center;
-            //bord.BorderThickness = new Windows.UI.Xaml.Thickness(4);
-            //bord.BorderBrush = new SolidColorBrush(Colors.Black);
-          
             if (Tic)
             {
                 SetX(canv);
-                //bord.Height = 20;
-                //bord.Width = 80;
-                //bord.Background = new SolidColorBrush(Colors.Black);
                 TicTac[i, j] = TicTacToe.Dagger;
             }
             else
             {
                 SetZero(canv);
-                //bord.Height = 80;
-                //bord.Width = 80;
-                //bord.CornerRadius = new Windows.UI.Xaml.CornerRadius(40);
                 TicTac[i, j] = TicTacToe.Toe;
             }
-            //canv.Children.Add(bord);
-            //Canvas.SetLeft(bord, 40);
-            //Canvas.SetTop(bord,  40);
             Tic = !Tic;
         }
 
         private void SetZero(Canvas canvas)
         {
             var path1 = new Windows.UI.Xaml.Shapes.Path();
-            //path1.Fill = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 204, 204, 255));
             var stroke = ((LinearGradientBrush)Page.Resources["colorZero"]);
             path1.Stroke = stroke;
+            path1.StrokeStartLineCap = PenLineCap.Round;
+            path1.StrokeEndLineCap = PenLineCap.Round;
             path1.StrokeThickness = 9;
             var geometryGroup1 = new GeometryGroup();
-
-
             var pathGeometry1 = new PathGeometry();
             var pathFigureCollection1 = new PathFigureCollection();
             var pathFigure1 = new PathFigure();
@@ -208,67 +191,83 @@ namespace Tic_tac_toe
 
             var pathSegmentCollection1 = new PathSegmentCollection();
             var pathSegment1 = new BezierSegment();
-            pathSegment1.Point1 = new Point(30, -2);
-            pathSegment1.Point2 = new Point(30, 25);
-            pathSegment1.Point3 = new Point(10, 30);
+            pathSegment1.Point1 = new Point(28, 8);
+            pathSegment1.Point2 = new Point(38.98, 11.99);
+            pathSegment1.Point3 = new Point(40.97, 23.97);
             pathSegmentCollection1.Add(pathSegment1);
 
             var pathSegment2 = new BezierSegment();
-            pathSegment2.Point1 = new Point(-20, 5);
-            pathSegment2.Point2 = new Point(-35, -20);
-            pathSegment2.Point3 = new Point(-13, -29);
+            pathSegment2.Point1 = new Point(39.96, 31.98);
+            pathSegment2.Point2 = new Point(29.98, 38.98);
+            pathSegment2.Point3 = new Point(18.98, 36.99);
             pathSegmentCollection1.Add(pathSegment2);
+
+            var pathSegment3 = new BezierSegment();
+            pathSegment3.Point1 = new Point(11.99, 35.98);
+            pathSegment3.Point2 = new Point(5.98, 29.99);
+            pathSegment3.Point3 = new Point(5.98, 20.98);
+            pathSegmentCollection1.Add(pathSegment3);
+
+            var pathSegment4 = new BezierSegment();
+            pathSegment4.Point1 = new Point(7.97, 14.97);
+            pathSegment4.Point2 = new Point(11.97, 8.97);
+            pathSegment4.Point3 = new Point(19.98, 8);
+            pathSegmentCollection1.Add(pathSegment4);
             pathFigure1.Segments = pathSegmentCollection1;
 
             geometryGroup1.Children.Add(pathGeometry1);
             path1.Data = geometryGroup1;
-            path1.StrokeStartLineCap = PenLineCap.Round;
-            path1.StrokeEndLineCap = PenLineCap.Round;
+
             canvas.Children.Add(path1);
             Canvas.SetLeft(path1, 20);
-            Canvas.SetTop(path1, 40);
+            Canvas.SetTop(path1, 20);
         }
 
 
         private void SetX(Canvas canvas)
         {
             var path1 = new Windows.UI.Xaml.Shapes.Path();
-            //path1.Fill = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 204, 204, 255));
-            var stroke = ((LinearGradientBrush)Page.Resources["colorZero"]);
+            var stroke = ((LinearGradientBrush)Page.Resources["colorX1"]);
             path1.Stroke = stroke;
+            path1.StrokeStartLineCap = PenLineCap.Round;
+            path1.StrokeEndLineCap = PenLineCap.Round;
             path1.StrokeThickness = 9;
             var geometryGroup1 = new GeometryGroup();
-
-
             var pathGeometry1 = new PathGeometry();
             var pathFigureCollection1 = new PathFigureCollection();
             var pathFigure1 = new PathFigure();
-            pathFigure1.IsClosed = true;
-            pathFigure1.StartPoint = new Windows.Foundation.Point(20, 8);
+            pathFigure1.StartPoint = new Windows.Foundation.Point(8.99, 6.98);
             pathFigureCollection1.Add(pathFigure1);
             pathGeometry1.Figures = pathFigureCollection1;
 
             var pathSegmentCollection1 = new PathSegmentCollection();
             var pathSegment1 = new BezierSegment();
-            pathSegment1.Point1 = new Point(30, -2);
-            pathSegment1.Point2 = new Point(30, 25);
-            pathSegment1.Point3 = new Point(10, 30);
+            pathSegment1.Point1 = new Point(19.99, 15.97);
+            pathSegment1.Point2 = new Point(30.99, 27.99);
+            pathSegment1.Point3 = new Point(38.96, 46.96);
             pathSegmentCollection1.Add(pathSegment1);
-
-            //var pathSegment2 = new BezierSegment();
-            //pathSegment2.Point1 = new Point(-20, 5);
-            //pathSegment2.Point2 = new Point(-35, -20);
-            //pathSegment2.Point3 = new Point(-13, -29);
-            //pathSegmentCollection1.Add(pathSegment2);
             pathFigure1.Segments = pathSegmentCollection1;
-
             geometryGroup1.Children.Add(pathGeometry1);
+
+            var pathGeometry2 = new PathGeometry();
+            var pathFigureCollection2 = new PathFigureCollection();
+            var pathFigure2 = new PathFigure();
+            pathFigure2.StartPoint = new Windows.Foundation.Point(39.99, 3.99);
+            pathFigureCollection2.Add(pathFigure2);
+            pathGeometry2.Figures = pathFigureCollection2;
+
+            var pathSegmentCollection2 = new PathSegmentCollection();
+            var pathSegment2 = new BezierSegment();
+            pathSegment2.Point1 = new Point(31.97, 19.98);
+            pathSegment2.Point2 = new Point(22.98, 31.96);
+            pathSegment2.Point3 = new Point(8.99, 40.99);
+            pathSegmentCollection2.Add(pathSegment2);
+            pathFigure2.Segments = pathSegmentCollection2;
+            geometryGroup1.Children.Add(pathGeometry2);
             path1.Data = geometryGroup1;
-            path1.StrokeStartLineCap = PenLineCap.Round;
-            path1.StrokeEndLineCap = PenLineCap.Round;
             canvas.Children.Add(path1);
             Canvas.SetLeft(path1, 20);
-            Canvas.SetTop(path1, 40);
+            Canvas.SetTop(path1, 20);
         }
     }
 }
